3ae2011a20ffdc5335ff46e91e9e16c1c6ef6bae
vatheiii <vateytouch13@gmail.com>
2026-03-30 14:17:06 +0700

    about

--- books/src/Pages/About/About.jsx
+++ books/src/Pages/About/About.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import "./About.css";
+import aboutImg from "../../images/about-img.jpg";
 
 const About = () => {
-	return <div>About</div>;
-};
+  return (
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+        </div>
 
-export default About;
+        <div >
+          <div >
+            <img src = {aboutImg} alt = "" />
+          </div>
+          <div>
+            <h2>About BookHub</h2>
+            <p>We are passionate about bringing stories and knowledge closer to everyone. Our platform offers a diverse collection of novels, academic resources, and bestselling books, all in one place. Whether you're reading for pleasure or learning something new, we make it easy to discover, explore, and enjoy your next favorite book anytime, anywhere.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
